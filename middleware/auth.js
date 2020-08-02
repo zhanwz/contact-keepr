b9e0f65d8bb2610bd0699fa6e58d78681fcf5c7b
Wei <wzhan@Weis-MacBook-Air.local>
2020-08-01 21:18:00 -0700

    Implement Users/Contacts/Auth routes and JWT token

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+    // Get token from the hreader
+    const token = req.header('x-auth-token');
+
+    // Check if not token
+    if (!token) {
+        return res.status(401).json({ msg: 'No token authorization denied' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+        req.user = decoded.user;
+        next();
+    } catch {
+        res.status(401).json({ msg: 'Token is not valid' });
+    }
+};
